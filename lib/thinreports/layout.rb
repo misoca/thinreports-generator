5387ced3ac5b91f22859b49e580d1ae1ffd46d4b
Katsuya <hidakatsuya@gmail.com>
2018-12-09 03:03:57 +0900

    Remove unused(refactored) codes

--- lib/thinreports/layout.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'simple_report/layout'
-
-module Thinreports
-  Layout = SimpleReport::Layout
-end
--- lib/thinreports/report.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'simple_report/report'
-
-module Thinreports
-  Report = SimpleReport::Report
-end
