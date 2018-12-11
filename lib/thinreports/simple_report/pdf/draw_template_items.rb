867be44c56c885c893cca29aec5dae31c8a9f813
Katsuya <hidakatsuya@gmail.com>
2018-12-12 02:23:15 +0900

    Refactor drawer for template item

--- lib/thinreports/simple_report/pdf/draw_template_items.rb
+++ lib/thinreports/simple_report/pdf/draw_template_items.rb
@@ -12,11 +12,11 @@ module Thinreports
             item = build_item_internal(item_attributes['type'], item_attributes)
 
             case item_attributes['type']
-            when 'text' then draw_text(item)
-            when 'image' then draw_image(item)
-            when 'rect' then draw_rect(item)
-            when 'ellipse' then draw_ellipse(item)
-            when 'line' then draw_line(item)
+            when 'text' then draw_shape_text(item)
+            when 'image' then draw_shape_image(item)
+            when 'rect' then draw_shape_rect(item)
+            when 'ellipse' then draw_shape_ellipse(item)
+            when 'line' then draw_shape_line(item)
             end
           end
         end
@@ -26,31 +26,6 @@ module Thinreports
         def build_item_internal(type, attributes)
           schema = Core::Shape::Format(type).new(attributes)
           Core::Shape::Interface(nil, schema).internal
-        end
-
-        # @param [Thinreports::Core::Shape::Basic::Internal] item
-        def draw_rect(item)
-          draw_shape_rect(item)
-        end
-
-        # @param [Thinreports::Core::Shape::Basic::Internal] item
-        def draw_ellipse(item)
-          draw_shape_ellipse(item)
-        end
-
-        # @param [Thinreports::Core::Shape::Basic::Internal] item
-        def draw_line(item)
-          draw_shape_line(item)
-        end
-
-        # @param [Thinreports::Core::Shape::Basic::Internal] item
-        def draw_text(item)
-          draw_shape_text(item)
-        end
-
-        # @param [Thinreports::Core::Shape::Basic::Internal] item
-        def draw_image(item)
-          draw_shape_image(item)
         end
 
         def drawable?(item_attributes)
